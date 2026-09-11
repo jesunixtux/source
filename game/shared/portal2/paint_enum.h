c0b6bc7665e6f892157a0f1fb440938439f7d205
Jesús Emilio <157801433+jesunixtux@users.noreply.github.com>
2026-09-11 15:36:26 -0300

    u2

---
+++ game/shared/portal2/paint_enum.h
@@ -0,0 +1,22 @@
+//========= Copyright © 1996-2010, Valve Corporation, All rights reserved. ============//
+//
+// Purpose: Defines the paint power types.
+//
+//=============================================================================//
+
+#ifndef PAINT_ENUM_H
+#define PAINT_ENUM_H
+
+enum PaintPowerType
+{
+	SPEED_POWER = 0,
+	BOUNCE_POWER,
+	CLEANSER_POWER,
+	REFLECT_POWER,
+	PORTAL_POWER,
+	VISUALIZER_POWER,
+	NO_POWER,
+	PAINT_POWER_TYPE_COUNT
+};
+
+#endif // ifndef PAINT_ENUM_H
