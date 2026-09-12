7440e17b15b65e1d8c9cadfdacaeeba79ae2e4e9
Jesús Emilio <157801433+jesunixtux@users.noreply.github.com>
2026-09-12 03:10:00 -0300

    w
    w

--- public/materialsystem/imesh.h
+++ public/materialsystem/imesh.h
@@ -38,30 +38,7 @@ typedef uint64 VertexFormat_t;
 //-----------------------------------------------------------------------------
 
 
-//struct ShaderStencilState_t Redundancia Se comentó para probar
 
-{
-	bool	m_bEnable;
-	int		m_nReferenceValue;
-	int		m_nWriteMask;
-	int		m_nTestMask;
-	int		m_CompareFunc;
-	int		m_PassOp;
-	int		m_FailOp;
-	int		m_ZFailOp;
-
-	ShaderStencilState_t()
-	{
-		m_bEnable = false;
-		m_nReferenceValue = 0;
-		m_nWriteMask = 0;
-		m_nTestMask = 0;
-		m_CompareFunc = 0;
-		m_PassOp = 0;
-		m_FailOp = 0;
-		m_ZFailOp = 0;
-	}
-};
 
 //-----------------------------------------------------------------------------
 // Interface to per-frame mesh data which can be cached across frames
